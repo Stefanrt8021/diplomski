d354879cfb9a090f99ce8c131ceca578e3998e7a
Stefan Zivanovic <stefanzivanovic565@gmail.com>
2025-06-18 19:47:28 +0200

    dopuna3

--- views/pages/about.php
+++ views/pages/about.php
@@ -11,9 +11,21 @@
                 <!-- //tittle heading -->
                 <div class="row">
                     <div class="col-lg-6 welcome-left">
-                        <h3>Stefan Živanović</h3>
+                        <!-- <h3>Stefan Živanović</h3>
                         <h4>RT-80/21</h4>
-                        <p>Ja sam Stefan Živanović, student sam Višer-a i ovo je moj diplomski rad iz predmeta Programiranje veb aplikacija.</p>
+                        <p>Ja sam Stefan Živanović, student sam Višer-a i ovo je moj diplomski rad iz predmeta Programiranje veb aplikacija.</p> -->
+                        <p><strong>Ime i prezime:</strong> Stefan Živanović</p>
+                        <p><strong>Broj indeksa:</strong> RT-80/21</p>
+                        <p><strong>Škola:</strong> Visoka škola elektrotehnike i računarstva strukovnih studija (Višer)</p>
+                        <p><strong>Smer:</strong> Računarska tehnika</p>
+
+                        <p>
+                        Zovem se Stefan Živanović i student sam Visoke škole elektrotehnike i računarstva. 
+                        Ovaj sajt je moj diplomski rad iz predmeta <em>Programiranje veb aplikacija</em>. 
+                        Tokom izrade rada koristio sam moderne tehnologije za frontend i backend razvoj, 
+                        kao što su HTML, CSS, JavaScript, PHP i MySQL. Cilj mi je bio da kroz praktičnu primenu znanja 
+                        prikažem razumevanje izrade dinamičkih i funkcionalnih veb aplikacija.
+                        </p>
                     </div>
                     <div class="col-lg-6 welcome-right-top mt-lg-0 mt-sm-5 mt-4">
                         <img src="assets/images/ja.jpg" class="img-fluid mx-auto" alt="Author ">
